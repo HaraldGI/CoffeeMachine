685493e76c7b31c6f35142e487fe97d58d860544
haraldgimse@gmail.com <haraldgimse@gmail.com>
2022-05-29 21:15:18 +0200

    Made TODO lists.

---
+++ main.py
@@ -0,0 +1,16 @@
+from menu import MENU
+from menu import resources
+
+# TODO: 6. Make while loop that turns the machine off.
+
+# TODO: 1. Prompt user what they would like: espresso/latte/cappuccino
+
+# TODO: 1.1 Check if there is enough resources for the different coffee types.
+
+# TODO: 2. When asked to report, pull out info from resources.
+
+# TODO: 3. Ask how many of each coin the user has. Quarters = 0.25, Dimes = 0.10, Nickles = 0.05 and Pennies = 0.01
+
+# TODO: 4. Check if transaction is sufficient. If there is enough money and/or enough resources.
+
+# TODO: 5. If successfull and enough resources and money. Make coffee and deduct from resources and give back change.
